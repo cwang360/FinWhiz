c1769e74d8e5a96c5ac91257c303fc2d87bdf306
cwang360 <35314533+cwang360@users.noreply.github.com>
2021-01-24 09:07:55 -0500

    Update insurance page

--- site/insurance/insurance.js
+++ site/insurance/insurance.js
@@ -1,16 +1,18 @@
 
 function getData(){
-
+    document.getElementById("subButton").innerHTML = "Loading...";
+    document.getElementById('insuranceGraph').src="";
          var medal = document.getElementById('medal').value;
          var plan = document.getElementById('plan').value;
+         var state = document.getElementById('state').value;
         var company = document.getElementById('company').value;
-          //Original
-        //   var url="https://www.wolframcloud.com/obj/2fb14b10-db1b-487e-ba72-985629bd9c5b?y="+year+"&m="+month+"&d="+day+"&p="+allPrices;
-        //   console.log(url);
+          var url="https://www.wolframcloud.com/obj/1548d63d-cc91-4914-90cf-ed51ab8d8f31?m="+medal+"&s="+state+"&g="+plan+"&c="+company;
+          console.log(url);
           console.log(medal);
           console.log(plan);
           console.log(company);
-        //   document.getElementById('insuranceGraph').src=url;
+        document.getElementById('insuranceGraph').src=url;
+        document.getElementById('subButton').innerHTML = "Submit";
 
        
 
